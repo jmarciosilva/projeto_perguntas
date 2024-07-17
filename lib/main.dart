019ae74f299da6202782d8a7e8a79af429c19296
jmarciosilva <jmarciosilva@gmail.com>
2024-07-17 19:38:40 -0300

    Adicionado  três RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,33 @@ class PerguntaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      "Qual é a sua cor favorita?",
+      "Qual é o seu animal favorito",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: (){},
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: (){},
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: (){},
+            ),
+          ],
+        ),
+
       ),
     );
   }
